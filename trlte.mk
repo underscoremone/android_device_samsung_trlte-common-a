ad53f2379cf9b34108d89fe0eb8e3cd5ac914443
Michael Wawrzyniak <mwawrzyniak@cmstactical.net>
2016-05-06 13:19:32 -0400

    trlte-common: update dalvik/hwui for 3GB
    Stock TouchWiz uses the 3GB values
    
    Change-Id: Iee0dc516cf36f3cfc2874e47c56360ec1eb7f690

--- trlte.mk
+++ trlte.mk
@@ -37,11 +37,10 @@ SCREEN_RATIO_PROPORTIONATE := true
 TARGET_SCREEN_ASPECT_RATIO := 16by9
 
 PRODUCT_PROPERTY_OVERRIDES += \
-   dalvik.vm.heapgrowthlimit=256m
 
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
